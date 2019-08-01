6db16f4404e6d631980d8284a6359925983ab320
Maksym Churkin <maksym.churkin@innovecs.com>
2019-08-01 13:35:17 +0300

    Some useful method were added to Field helper

--- Field/FieldHelperAbstract.php
+++ Field/FieldHelperAbstract.php
@@ -4,6 +4,8 @@ namespace Garlic\GraphQL\Field;
 
 use Youshido\GraphQLBundle\Field\AbstractContainerAwareField;
 use Youshido\GraphQL\Type\ListType\ListType;
+use Youshido\GraphQLExtension\Type\PagingParamsType;
+use Youshido\GraphQLExtension\Type\Sorting\SortingParamsType;
 
 abstract class FieldHelperAbstract extends AbstractContainerAwareField
 {
@@ -19,6 +21,7 @@ abstract class FieldHelperAbstract extends AbstractContainerAwareField
         if (isset($args[$name])) {
             $result = $args[$name];
             unset($args[$name]);
+
             return $result;
         }
 
@@ -40,4 +43,47 @@ abstract class FieldHelperAbstract extends AbstractContainerAwareField
 
         return $result;
     }
+
+    /**
+     * Cut and map pagination and sort arguments
+     *
+     * @param array $args
+     * @return array
+     */
+    protected function getPagingSortArgumentsData(array $args)
+    {
+        $pagination = (array)$this->cutArgument('paging', $args);
+
+        return [
+            'sort' => (array)$this->cutArgument('sort', $args) ?? ['id' => 'ASC'],
+            'limit' => $pagination['limit'] ?? (int)getenv('DEFAULT_RESULT_LIMIT'),
+            'offset' => $pagination['offset'] ?? 0,
+        ];
+    }
+
+    /**
+     * Merge paging/sort arguments
+     *
+     * @param $type
+     * @param array $args
+     * @return array
+     */
+    protected function mergePagingSortArguments($type, array $args = [])
+    {
+        $paging = new PagingParamsType();
+        $sorting = new SortingParamsType($type->init(), array_keys($type->getArguments()));
+        if(empty($args)) {
+            $args = $type->getArguments();
+        }
+
+        return array_merge(
+            $args,
+            [
+                'paging' => $paging->getType(),
+                'sort' => $sorting->getType(),
+            ]
+        );
+    }
 }
+
+
